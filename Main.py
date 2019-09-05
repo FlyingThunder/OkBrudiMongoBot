7bca52df060c49668392587e38948ffa692a4312
FlyingThunder <laurens.peschko@gmail.com>
2019-09-05 14:04:33 +0200

    läuft brudi

--- Main.py
+++ Main.py
@@ -1,10 +1,35 @@
+import time
 import tweepy
-# Authenticate to Twitter
-auth = tweepy.OAuthHandler("CONSUMER_KEY", "CONSUMER_SECRET")
-auth.set_access_token("ACCESS_TOKEN", "ACCESS_TOKEN_SECRET")
+import random
+import praw
+import urllib.request
+import os
 
-# Create API object
-api = tweepy.API(auth)
 
-# Create a tweet
-api.update_status("Hello Tweepy")
+
+def Mainbot():
+    reddit = praw.Reddit(client_id='SECRET',
+                          client_secret='SECRET',
+                          user_agent='RedditFetchBot by FlyingThunder')
+
+    def Tweet(postinfo):
+        auth = tweepy.OAuthHandler("SECRET", "SECRET")
+        auth.set_access_token("SECRET",
+                              "SECRET")
+        api = tweepy.API(auth)
+        api.update_with_media("local-filename.jpg", postinfo)
+        os.remove("local-filename.jpg")
+
+
+    sub = reddit.subreddit('okbrudimongo').new(limit=1000)
+    random_post_number = random.randint(0,1000)
+
+
+    for i,post in enumerate(sub):
+        if i==random_post_number:
+            urllib.request.urlretrieve(post.url, "local-filename.jpg")
+            Tweet(postinfo=post.title+"(https://www.reddit.com" + post.permalink+")")
+
+while True:
+    Mainbot()
+    time.sleep(3600)
